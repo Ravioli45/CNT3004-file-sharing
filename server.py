db80fff609a0fa94ea316395c6b5689fd9e00f60
Joseph Elibri <elibrij@gmail.com>
2024-11-18 12:09:57 -0500

    server now acts as an echo server

--- server.py
+++ server.py
@@ -1,21 +1,41 @@
 import threading
 import socket
 
-IP = socket.gethostbyname(socket.gethostname())
+IP = "127.0.0.1" #socket.gethostbyname(socket.gethostname())
 PORT = 3300
 BUFFER_SIZE = 1024
 FORMAT = "utf-8"
 
 ADDR = (IP, PORT)
 
-def handle_connection(connection, address):
+def logon(connection: socket) -> bool:
+    data = connection.recv(1024).decode(FORMAT)
+    return data == "LOGON"
+
+def handle_connection(connection: socket, address):
 
     print(f"[*] Established connection: {address}")
 
+    if not logon(connection):
+        connection.send("ERR".encode(FORMAT))
+        connection.close()
+        print(f"[*] Connection closed: {address}")
+        return
+
     connection.send("OK".encode(FORMAT))
 
+    while True:
+        data = connection.recv(BUFFER_SIZE).decode("utf-8")
+
+        command = data.split(' ')
+
+        if data == "LOGOUT":
+            break
+
+        connection.send(f"---> {data}".encode("utf-8"))
+
     connection.close()
-    print(f"[*] Connection close: {address}")
+    print(f"[*] Connection closed: {address}")
 
 
 def main():
