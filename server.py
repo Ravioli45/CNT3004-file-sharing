c53438a152ae932a0f322582179f7b191e9f0d7c
Joseph Elibri <elibrij@gmail.com>
2024-11-19 21:08:15 -0500

    added file download from server

--- server.py
+++ server.py
@@ -10,11 +10,17 @@ BASE_DIR = Path("./data").resolve()
 
 ADDR = (IP, PORT)
 
-def send_ok(connection: socket, message = ""):
-    connection.send(f"OK {message}".encode('utf-8'))
+def send_ok(connection: socket, message = None):
+    if message is None:
+        connection.send("OK".encode('utf-8'))
+    else:
+        connection.send(f"OK {message}".encode('utf-8'))
 
-def send_err(connection: socket, message: str):
-    connection.send(f"ERR {message}".encode('utf-8'))
+def send_err(connection: socket, message = None):
+    if message is None:
+        connection.send("ERR".encode('utf-8'))
+    else:
+        connection.send(f"ERR {message}".encode('utf-8'))
 
 def is_valid_path(other_path: Path) -> bool:
     return other_path.is_relative_to(BASE_DIR)
@@ -50,6 +56,28 @@ def handle_upload(connection: socket, params: list[str]):
     
     send_ok(connection)
 
+def handle_download(connection: socket, params: list[str]):
+    
+    file_path = (BASE_DIR / params[0]).resolve()
+
+    if not is_valid_path(file_path) or not file_path.is_file():
+        send_err(connection)
+        return
+    
+    # send OK (file size in bytes)
+    send_ok(connection, file_path.stat().st_size)
+
+    # awaits OK from client before sending file
+    data = connection.recv(1024).decode(FORMAT)
+
+    if not data == "OK":
+        send_err(connection)
+        return
+
+    with file_path.open('rb') as file:
+        connection.sendfile(file)
+    
+    send_ok(connection)
 
 def handle_connection(connection: socket, address):
 
@@ -64,7 +92,7 @@ def handle_connection(connection: socket, address):
     send_ok(connection)
 
     while True:
-        data = connection.recv(BUFFER_SIZE).decode("utf-8")
+        data = connection.recv(BUFFER_SIZE).decode(FORMAT)
 
         command = data.split(' ')
 
@@ -73,8 +101,9 @@ def handle_connection(connection: socket, address):
                 break
             case "UPLOAD":
                 handle_upload(connection, command[1:])
+            case "DOWNLOAD":
+                handle_download(connection, command[1:])
 
-        connection.send(f"---> {data}".encode("utf-8"))
 
     connection.close()
     print(f"[*] Connection closed: {address}")
