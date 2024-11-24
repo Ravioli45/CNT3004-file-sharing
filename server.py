bed9c5e5601fc8637ef1308ae421ff1f4f5612b6
Joseph Elibri <elibrij@gmail.com>
2024-11-24 17:32:45 -0500

    added socket option to reuse address

--- server.py
+++ server.py
@@ -304,6 +304,7 @@ def main():
 
     print("[*] Starting Server...")
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind(ADDR)
     server.listen()
 
